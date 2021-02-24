8c45f469cb5da6c900e4de72fc55ba648b2fdfff
Marcin Medryk <medryk.kontakt@gmail.com>
2021-02-24 14:37:01 +0100

    wip

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import AppLoading from "expo-app-loading";
+import Svg, { Path } from "react-native-svg";
+import * as Font from "expo-font";
 import {
   StyleSheet,
   Text,
@@ -8,10 +10,25 @@ import {
   ScrollView,
   SafeAreaView,
   Image,
+  Dimensions,
 } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   let [posts, setPosts] = React.useState([]);
+  let [fontsLoaded, setFontsLoaded] = React.useState(false);
+
+  let windowWidth = Dimensions.get("window").width;
+  let windowHeight = Dimensions.get("window").height;
+
+  async function getFonts() {
+    await fetchData();
+    await Font.loadAsync({
+      "Gilroy-Light": require("./assets/Fonts/Gilroy-Light.otf"),
+      "Gilroy-ExtraBold": require("./assets/Fonts/Gilroy-ExtraBold.otf"),
+    });
+    setFontsLoaded(true);
+  }
 
   let textTooLong = (text) => {
     if (text.length > 100) {
@@ -37,32 +54,52 @@ export default function App() {
     }
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Text>Hello</Text>
-        {posts.map((post) => (
-          <View key={post.id} style={styles.postStyle}>
-            <View style={[styles.column, styles.grow]}>
-              <Text style={styles.postTitleStyle}>{post.title}</Text>
-              <Text style={styles.postBodyStyle}>{textTooLong(post.body)}</Text>
-            </View>
-            <View style={[styles.column, styles.center]}>
-              <View style={[styles.row, styles.nextButton]}>
-                <View style={styles.postButtonStyle}>
-                  <Image
-                    source={require("./assets/arrow-point-to-right.png")}
-                    style={styles.arrowStyle}
-                  />
+  if (fontsLoaded) {
+    return (
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <ScrollView>
+            <Text>Hello</Text>
+            {posts.map((post) => (
+              <View key={post.id} style={styles.postStyle}>
+                <View style={[styles.column, styles.grow]}>
+                  <Text style={styles.postTitleStyle}>{post.title}</Text>
+                  <Text style={styles.postBodyStyle}>
+                    {textTooLong(post.body)}
+                  </Text>
+                </View>
+                <View style={[styles.column, styles.center]}>
+                  <View style={[styles.row, styles.nextButton]}>
+                    <View style={styles.postButtonStyle}>
+                      <Svg
+                        style={styles.arrowStyle}
+                        width="5"
+                        height="10"
+                        viewBox="0 0 5 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <Path
+                          d="M0.875 1.625L4.25 5L0.875 8.375"
+                          stroke="#466BC9"
+                          stroke-width="1.4"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </Svg>
+                    </View>
+                  </View>
                 </View>
               </View>
-            </View>
-          </View>
-        ))}
-        <Button title="Fetch" onPress={fetchData}></Button>
-      </ScrollView>
-    </SafeAreaView>
-  );
+            ))}
+            <Button title="Fetch" onPress={fetchData}></Button>
+          </ScrollView>
+        </SafeAreaView>
+      </NavigationContainer>
+    );
+  } else {
+    return <AppLoading start={getFonts()} />;
+  }
 }
 
 const styles = StyleSheet.create({
@@ -78,12 +115,20 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   postTitleStyle: {
+    fontFamily: "Gilroy-Light",
+    fontSize: 14,
+    lineHeight: 21,
+    letterSpacing: 0.2,
     color: "#001524",
     marginLeft: 24,
     marginTop: 24,
     marginBottom: 4,
   },
   postBodyStyle: {
+    fontFamily: "Gilroy-Light",
+    fontSize: 14,
+    lineHeight: 21,
+    letterSpacing: 0.2,
     color: "#A0ABB3",
     marginLeft: 24,
     marginBottom: 24,
@@ -112,9 +157,8 @@ const styles = StyleSheet.create({
     flexDirection: "column",
   },
   arrowStyle: {
-    marginLeft: 7.5,
-    marginTop: 6.5,
-    height: 8,
-    width: 8,
+    backgroundColor: "#ECF0FA",
+    marginLeft: 8.5,
+    marginTop: 6,
   },
 });
---
+++ screens/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  SafeAreaView,
+  Image,
+  Dimensions,
+} from "react-native";
+
+let Home = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default Home;
