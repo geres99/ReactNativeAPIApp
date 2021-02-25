83fe4220bd0fe799fceeac145c0c59928052f4ec
Marcin Medryk <medryk.kontakt@gmail.com>
2021-02-25 11:29:34 +0100

    wip

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./screens/Home";
 import NewsDetails from "./screens/NewsDetails";
-import BackButton from "./BackButton";
+import BackButton from "./components/BackButton";
 
 const Stack = createStackNavigator();
 
@@ -24,17 +24,40 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="News List" component={Home} />
+        <Stack.Screen
+          name="News List"
+          component={Home}
+          options={{
+            title: "News list",
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: "#FFFFFF",
+              height: 100,
+            },
+            headerTintColor: "#001524",
+            headerTitleStyle: {
+              fontFamily: "Gilroy-Light",
+              fontSize: 16,
+              lineHeight: 24,
+              fontWeight: "bold",
+            },
+          }}
+        />
         <Stack.Screen
           name="News Details"
           component={NewsDetails}
           options={{
-            title: "My home",
+            title: "News details",
+            headerTitleAlign: "center",
             headerStyle: {
               backgroundColor: "#FFFFFF",
+              height: 100,
             },
             headerTintColor: "#001524",
             headerTitleStyle: {
+              fontFamily: "Gilroy-Light",
+              fontSize: 16,
+              lineHeight: 24,
               fontWeight: "bold",
             },
             headerBackImage: () => <BackButton />,
---
+++ screens/Album.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+
+let Album = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default Album;
